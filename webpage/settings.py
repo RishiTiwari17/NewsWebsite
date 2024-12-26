3d36897e0ca7af3e6c736f815b85f8bc5e7b61c7
unknown <rishi00as@gmail.com>
2024-12-26 20:57:02 +0530

    six commit

--- webpage/settings.py
+++ webpage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure--*ni^j-kx*pv)*a84reo_wypz$sib3ke6*#^ualey%!bvc=j-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
